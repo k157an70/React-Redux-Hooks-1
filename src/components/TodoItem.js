ac2fb628fe49608ed951a5990a192eeb324f6e1e
kistantopemalang <46606587+kistantopemalang@users.noreply.github.com>
2019-06-22 11:23:03 +0700

    Add files via upload

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,1 +1,27 @@
+import React from 'react';
+import { Text, TouchableOpacity } from 'react-native';
+
+const TodoItem =  ({ todo, toggleTd, deleteTd }) => {
+   return (
+      <TouchableOpacity
+         onPress={toggleTd}
+         style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}
+      >
+         <Text
+            style={[
+               { fontSize: 20, lineHeight: 40 },
+               { textDecorationLine: todo.completed ? 'line-through' : 'none' }
+            ]}
+         >
+            {todo.name}
+         </Text>
+         <TouchableOpacity onPress={deleteTd}>
+            <Text style={{ fontSize: 20}}>x</Text>
+         </TouchableOpacity>
+      </TouchableOpacity>
+   )
+}
+
+export default TodoItem;
+
 
