33ab18fc66b0ceb701dd82146466c7b0fc59ddec
kistantopemalang <46606587+kistantopemalang@users.noreply.github.com>
2019-06-22 11:23:43 +0700

    Add files via upload

---
+++ src/redux.js
@@ -0,0 +1,50 @@
+import { createStore } from 'redux'
+// actions, reducers, store
+//acionTypes
+const ADD_TODO = 'ADD_TODO';
+const DELETE_TODO = 'DELETE_TODO';
+const TOGGLE_TODO = 'TOGGLE_TODO';
+//action
+export const addTodoAction = todo => ({
+   type: ADD_TODO,
+   payload: todo
+})
+export const deleteTodoAction = id => ({
+   type: DELETE_TODO,
+   payload: id
+})
+export const toggleTodoAction = id => ({
+   type: TOGGLE_TODO,
+   payload: id
+})
+//reducers
+const initialState = {
+   todos: [
+      { id: 1, name: 'Learn Redux', completed: false },
+      { id: 2, name: 'Learn React-Redux', completed: true },
+   ]
+}
+const reducer = (state = initialState, action) => {
+   switch (action.type) {
+      case ADD_TODO:
+         return {
+            ...state,
+            todos: [...state.todos, action.payload]
+         }
+      case DELETE_TODO:
+         return {
+            ...state,
+            todos: state.todos.filter(todo => todo.id !== action.payload)
+         }
+      case TOGGLE_TODO:
+         return {
+            ...state,
+            todos: state.todos.map(todo => todo.id === action.payload ? { ...todo, completed: !todo.completed } : todo)
+         }
+      default:
+         return state;
+   }
+}
+
+//store
+export const store = createStore(reducer);
